e913ef471f3dd83266f3d63d41085a5030b311cb
StanislavWEB <stanisov@gmail.com>
2014-12-10 12:38:02 +0200

    Commit fixed 5

--- src/Searcher/Validator.php
+++ src/Searcher/Validator.php
@@ -260,7 +260,7 @@ class Validator {
 
 			$sort = array_map('strtolower', $sort);
 
-			if(empty($diff = array_diff($sort, $this->_sort)))
+			if(empty($diff = array_diff($sort, $this->_sort)) === false)
 				throw new Exceptions\ColumnException(Exceptions\ColumnException::ORDER_TYPES_DOES_NOT_EXISTS, [
 					$not, $table, $metaData->getAttributes($model)]);
 
