71f96725b942c042b4601d2907222f5afd0954e9
Stanislav WEB <stanisov@gmail.com>
2014-12-15 22:55:28 +0200

    Coverage fixed

---
+++ phpunit/log/coverage.php
@@ -0,0 +1,752 @@
+<?php
+$coverage = new PHP_CodeCoverage;
+$coverage->setData(array (
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-searcher/src/Searcher/Validator.php' => 
+  array (
+    78 => 
+    array (
+    ),
+    79 => 
+    array (
+    ),
+    84 => 
+    array (
+    ),
+    85 => 
+    array (
+    ),
+    87 => 
+    array (
+    ),
+    88 => 
+    array (
+    ),
+    89 => 
+    array (
+    ),
+    90 => 
+    array (
+    ),
+    91 => 
+    array (
+    ),
+    94 => 
+    array (
+    ),
+    95 => NULL,
+    105 => 
+    array (
+      0 => 'Phalcon\\Phpunit\\Searcher\\ValidatorTest::testLimits',
+    ),
+    106 => 
+    array (
+      0 => 'Phalcon\\Phpunit\\Searcher\\ValidatorTest::testLimits',
+    ),
+    108 => 
+    array (
+      0 => 'Phalcon\\Phpunit\\Searcher\\ValidatorTest::testLimits',
+    ),
+    109 => 
+    array (
+      0 => 'Phalcon\\Phpunit\\Searcher\\ValidatorTest::testLimits',
+    ),
+    110 => NULL,
+    120 => 
+    array (
+      0 => 'Phalcon\\Phpunit\\Searcher\\ValidatorTest::testLimits',
+    ),
+    121 => 
+    array (
+      0 => 'Phalcon\\Phpunit\\Searcher\\ValidatorTest::testLimits',
+    ),
+    123 => 
+    array (
+      0 => 'Phalcon\\Phpunit\\Searcher\\ValidatorTest::testLimits',
+    ),
+    124 => 
+    array (
+      0 => 'Phalcon\\Phpunit\\Searcher\\ValidatorTest::testLimits',
+    ),
+    125 => NULL,
+    135 => 
+    array (
+    ),
+    136 => 
+    array (
+    ),
+    137 => 
+    array (
+    ),
+    138 => NULL,
+    149 => 
+    array (
+    ),
+    150 => 
+    array (
+    ),
+    151 => 
+    array (
+    ),
+    152 => NULL,
+    163 => 
+    array (
+    ),
+    164 => 
+    array (
+    ),
+    166 => 
+    array (
+    ),
+    167 => NULL,
+    178 => 
+    array (
+    ),
+    179 => 
+    array (
+    ),
+    181 => 
+    array (
+    ),
+    182 => NULL,
+    193 => 
+    array (
+    ),
+    194 => 
+    array (
+    ),
+    196 => 
+    array (
+    ),
+    197 => NULL,
+    209 => 
+    array (
+    ),
+    210 => 
+    array (
+    ),
+    211 => 
+    array (
+    ),
+    212 => NULL,
+    226 => 
+    array (
+    ),
+    230 => 
+    array (
+    ),
+    231 => 
+    array (
+    ),
+    233 => 
+    array (
+    ),
+    237 => 
+    array (
+    ),
+    238 => 
+    array (
+    ),
+    241 => 
+    array (
+    ),
+    244 => 
+    array (
+    ),
+    248 => 
+    array (
+    ),
+    250 => 
+    array (
+    ),
+    251 => 
+    array (
+    ),
+    254 => 
+    array (
+    ),
+    255 => 
+    array (
+    ),
+    256 => 
+    array (
+    ),
+    257 => 
+    array (
+    ),
+    258 => 
+    array (
+    ),
+    259 => 
+    array (
+    ),
+    260 => NULL,
+    274 => 
+    array (
+    ),
+    278 => 
+    array (
+    ),
+    279 => 
+    array (
+    ),
+    281 => 
+    array (
+    ),
+    285 => 
+    array (
+    ),
+    286 => 
+    array (
+    ),
+    290 => 
+    array (
+    ),
+    292 => 
+    array (
+    ),
+    293 => 
+    array (
+    ),
+    295 => 
+    array (
+    ),
+    296 => 
+    array (
+    ),
+    298 => 
+    array (
+    ),
+    299 => 
+    array (
+    ),
+    300 => 
+    array (
+    ),
+    301 => 
+    array (
+    ),
+    302 => NULL,
+    314 => 
+    array (
+    ),
+    315 => 
+    array (
+    ),
+    316 => NULL,
+    317 => 
+    array (
+    ),
+    318 => NULL,
+  ),
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-searcher/src/Searcher/Aware/ExceptionInterface.php' => 
+  array (
+  ),
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-searcher/src/Searcher/Builder.php' => 
+  array (
+    51 => 
+    array (
+    ),
+    52 => 
+    array (
+    ),
+    53 => 
+    array (
+    ),
+    61 => 
+    array (
+    ),
+    62 => 
+    array (
+    ),
+    70 => 
+    array (
+    ),
+    71 => NULL,
+    80 => 
+    array (
+    ),
+    83 => 
+    array (
+    ),
+    85 => 
+    array (
+    ),
+    86 => 
+    array (
+    ),
+    87 => NULL,
+    97 => 
+    array (
+    ),
+    98 => 
+    array (
+    ),
+    100 => 
+    array (
+    ),
+    101 => 
+    array (
+    ),
+    102 => 
+    array (
+    ),
+    103 => 
+    array (
+    ),
+    105 => 
+    array (
+    ),
+    106 => 
+    array (
+    ),
+    107 => 
+    array (
+    ),
+    108 => NULL,
+    119 => 
+    array (
+    ),
+    120 => 
+    array (
+    ),
+    122 => 
+    array (
+    ),
+    124 => 
+    array (
+    ),
+    126 => 
+    array (
+    ),
+    127 => 
+    array (
+    ),
+    128 => 
+    array (
+    ),
+    129 => 
+    array (
+    ),
+    130 => 
+    array (
+    ),
+    131 => 
+    array (
+    ),
+    132 => NULL,
+    141 => 
+    array (
+    ),
+    142 => 
+    array (
+    ),
+    144 => 
+    array (
+    ),
+    145 => 
+    array (
+    ),
+    146 => 
+    array (
+    ),
+    147 => 
+    array (
+    ),
+    148 => 
+    array (
+    ),
+    150 => 
+    array (
+    ),
+    151 => 
+    array (
+    ),
+    152 => 
+    array (
+    ),
+    155 => 
+    array (
+    ),
+    157 => 
+    array (
+    ),
+    158 => NULL,
+    168 => 
+    array (
+    ),
+    169 => 
+    array (
+    ),
+    171 => 
+    array (
+    ),
+    173 => 
+    array (
+    ),
+    174 => 
+    array (
+    ),
+    175 => 
+    array (
+    ),
+    176 => 
+    array (
+    ),
+    177 => 
+    array (
+    ),
+    178 => NULL,
+    191 => 
+    array (
+    ),
+    192 => 
+    array (
+    ),
+    193 => 
+    array (
+    ),
+    194 => 
+    array (
+    ),
+    196 => 
+    array (
+    ),
+    198 => 
+    array (
+    ),
+    202 => 
+    array (
+    ),
+    203 => 
+    array (
+    ),
+    205 => 
+    array (
+    ),
+    207 => 
+    array (
+    ),
+    208 => NULL,
+    221 => 
+    array (
+    ),
+    223 => 
+    array (
+    ),
+    225 => 
+    array (
+    ),
+    226 => 
+    array (
+    ),
+    227 => 
+    array (
+    ),
+    229 => 
+    array (
+    ),
+    231 => 
+    array (
+    ),
+    232 => 
+    array (
+    ),
+    234 => 
+    array (
+    ),
+    235 => 
+    array (
+    ),
+    236 => 
+    array (
+    ),
+    237 => 
+    array (
+    ),
+    238 => 
+    array (
+    ),
+    239 => 
+    array (
+    ),
+    240 => 
+    array (
+    ),
+    241 => 
+    array (
+    ),
+    242 => 
+    array (
+    ),
+    243 => 
+    array (
+    ),
+    244 => 
+    array (
+    ),
+    245 => NULL,
+    246 => 
+    array (
+    ),
+    247 => 
+    array (
+    ),
+    249 => 
+    array (
+    ),
+  ),
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-searcher/src/Searcher/Exceptions/Column.php' => 
+  array (
+    41 => 
+    array (
+    ),
+    44 => 
+    array (
+    ),
+    45 => 
+    array (
+    ),
+    48 => 
+    array (
+    ),
+    50 => 
+    array (
+    ),
+    53 => 
+    array (
+    ),
+    55 => 
+    array (
+    ),
+    58 => 
+    array (
+    ),
+    59 => 
+    array (
+    ),
+    61 => 
+    array (
+    ),
+    63 => 
+    array (
+    ),
+    64 => NULL,
+    73 => 
+    array (
+    ),
+    74 => NULL,
+  ),
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-searcher/src/Searcher/Exceptions/DataType.php' => 
+  array (
+    41 => 
+    array (
+    ),
+    42 => 
+    array (
+    ),
+    44 => 
+    array (
+    ),
+    45 => NULL,
+    54 => 
+    array (
+    ),
+    55 => NULL,
+  ),
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-searcher/src/Searcher/Exceptions/InvalidLength.php' => 
+  array (
+    35 => 
+    array (
+    ),
+    37 => 
+    array (
+    ),
+    38 => NULL,
+    47 => 
+    array (
+    ),
+    48 => NULL,
+  ),
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-searcher/src/Searcher/Exceptions/Model.php' => 
+  array (
+    41 => 
+    array (
+    ),
+    44 => 
+    array (
+    ),
+    46 => 
+    array (
+    ),
+    48 => 
+    array (
+    ),
+    50 => 
+    array (
+    ),
+    51 => NULL,
+    60 => 
+    array (
+    ),
+    61 => NULL,
+  ),
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-searcher/src/Searcher/Factories/ExceptionFactory.php' => 
+  array (
+    31 => 
+    array (
+    ),
+    32 => 
+    array (
+    ),
+    34 => 
+    array (
+    ),
+    37 => 
+    array (
+    ),
+    38 => NULL,
+  ),
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-searcher/src/Searcher/Searcher.php' => 
+  array (
+    42 => 
+    array (
+    ),
+    43 => 
+    array (
+    ),
+    53 => 
+    array (
+    ),
+    54 => 
+    array (
+    ),
+    55 => NULL,
+    65 => 
+    array (
+    ),
+    66 => 
+    array (
+    ),
+    67 => NULL,
+    80 => 
+    array (
+    ),
+    81 => 
+    array (
+    ),
+    82 => NULL,
+    107 => 
+    array (
+    ),
+    108 => 
+    array (
+    ),
+    109 => 
+    array (
+    ),
+    111 => 
+    array (
+    ),
+    112 => NULL,
+    132 => 
+    array (
+    ),
+    133 => 
+    array (
+    ),
+    134 => 
+    array (
+    ),
+    135 => 
+    array (
+    ),
+    136 => NULL,
+    157 => 
+    array (
+    ),
+    158 => 
+    array (
+    ),
+    159 => 
+    array (
+    ),
+    161 => 
+    array (
+    ),
+    162 => NULL,
+    180 => 
+    array (
+    ),
+    181 => 
+    array (
+    ),
+    183 => 
+    array (
+    ),
+    184 => 
+    array (
+    ),
+    185 => 
+    array (
+    ),
+    186 => NULL,
+    201 => 
+    array (
+    ),
+    203 => 
+    array (
+    ),
+    204 => 
+    array (
+    ),
+    206 => 
+    array (
+    ),
+    207 => 
+    array (
+    ),
+    208 => NULL,
+    216 => 
+    array (
+    ),
+    217 => NULL,
+    230 => 
+    array (
+    ),
+    231 => 
+    array (
+    ),
+    232 => 
+    array (
+    ),
+    233 => 
+    array (
+    ),
+    236 => 
+    array (
+    ),
+  ),
+));
+$coverage->setTests(array (
+  'Phalcon\\Phpunit\\Searcher\\ValidatorTest::testProperties' => 0,
+  'Phalcon\\Phpunit\\Searcher\\ValidatorTest::testLimits' => 0,
+  'UNCOVERED_FILES_FROM_WHITELIST' => NULL,
+));
+
+$filter = $coverage->filter();
+$filter->setBlacklistedFiles(array (
+  '/tmp/ide-phpunit.php' => true,
+));
+$filter->setWhitelistedFiles(array (
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-searcher/src/Searcher/Aware/ExceptionInterface.php' => true,
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-searcher/src/Searcher/Builder.php' => true,
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-searcher/src/Searcher/Exceptions/Column.php' => true,
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-searcher/src/Searcher/Exceptions/DataType.php' => true,
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-searcher/src/Searcher/Exceptions/InvalidLength.php' => true,
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-searcher/src/Searcher/Exceptions/Model.php' => true,
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-searcher/src/Searcher/Factories/ExceptionFactory.php' => true,
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-searcher/src/Searcher/Searcher.php' => true,
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-searcher/src/Searcher/Validator.php' => true,
+));
+
+return $coverage;
