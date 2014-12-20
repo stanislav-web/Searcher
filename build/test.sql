b77db58ebb19dbcfdfbb753b5147eb6845b68a20
Stanislav WEB <stanisov@gmail.com>
2014-12-20 15:11:51 +0200

    Test MySQL

--- build/test.sql
+++ build/test.sql
@@ -15,7 +15,7 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-USE phalcon.local;
+USE `my_project`;
 
 --
 -- Table structure for table `categories`
